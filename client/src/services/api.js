92ccdd3c1bf9cfcd34b4848dbfef03204446eb35
Abhay Singh Tomar <abhaysinghtomar97@gmail.com>
2026-04-09 17:45:47 +0530

    debug: /api route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL + "/api",  //backend api
+    baseURL: import.meta.env.VITE_API_URL ,  //backend api
 });
 
 // Automatically attach the JWT token to every request if the user is logged in
