e04fcb14d869604e6cb8c6b6ac81c85a8979edab
Abhay Singh Tomar <abhaysinghtomar97@gmail.com>
2026-04-11 21:11:33 +0530

    debug: smtp

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 // Setup Nodemailer 
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Use `true` for port 465, `false` for all other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD 
