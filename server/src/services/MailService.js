6d2d4e5076633cddbdb399ea1a2d0b60192675ba
Abhay Singh Tomar <abhaysinghtomar97@gmail.com>
2026-04-15 21:45:11 +0530

    .

--- server/src/services/MailService.js
+++ server/src/services/MailService.js
@@ -8,6 +8,7 @@ class MailService {
             host: process.env.SMTP_HOST,
             port: parseInt(process.env.SMTP_PORT, 10),
             secure: process.env.SMTP_SECURE === 'true', 
+            tls: { rejectUnauthorized: false },
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
